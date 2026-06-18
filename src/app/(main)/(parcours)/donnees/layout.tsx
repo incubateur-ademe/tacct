e609c69fd242639835063dd35957bf873c1a9d40
salamèche <antoine.conegero@gmail.com>
2026-06-18 10:07:56 +0200

    fix: useSearchParam in suspense

--- src/app/(main)/(parcours)/donnees/layout.tsx
+++ src/app/(main)/(parcours)/donnees/layout.tsx
@@ -4,7 +4,7 @@ import { MenuLateral } from '@/components/ui/MenuLateral';
 import { useSearchParams } from 'next/navigation';
 import { Suspense, useState, type PropsWithChildren } from 'react';
 
-const ExplorerTerritoireLayout = ({ children }: PropsWithChildren) => {
+const ExplorerTerritoireLayoutInner = ({ children }: PropsWithChildren) => {
   const searchParams = useSearchParams();
   const code = searchParams.get('code')!;
   const [isMenuCollapsed, setIsMenuCollapsed] = useState<boolean>(false);
@@ -18,28 +18,30 @@ const ExplorerTerritoireLayout = ({ children }: PropsWithChildren) => {
   ) {
     return (
       <div className="py-12">
-        <Suspense>
-          <DonneesIndisponiblesOutreMer />;
-        </Suspense>
+        <DonneesIndisponiblesOutreMer />
       </div>
     );
   }
 
   return (
-    <Suspense>
-      <div className="flex min-h-screen">
-        {/* Menu latéral fixe */}
-        <MenuLateral
-          isCollapsed={isMenuCollapsed}
-          onToggleCollapse={setIsMenuCollapsed}
-        />
-        {/* Contenu principal */}
-        <div
-          className={`flex-1 flex flex-col transition-all duration-500 ease-in-out ${isMenuCollapsed ? 'ml-[50px]' : 'ml-[322px]'}`}
-        >
-          <div className="flex-1">{children}</div>
-        </div>
+    <div className="flex min-h-screen">
+      <MenuLateral
+        isCollapsed={isMenuCollapsed}
+        onToggleCollapse={setIsMenuCollapsed}
+      />
+      <div
+        className={`flex-1 flex flex-col transition-all duration-500 ease-in-out ${isMenuCollapsed ? 'ml-[50px]' : 'ml-[322px]'}`}
+      >
+        <div className="flex-1">{children}</div>
       </div>
+    </div>
+  );
+};
+
+const ExplorerTerritoireLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <Suspense>
+      <ExplorerTerritoireLayoutInner>{children}</ExplorerTerritoireLayoutInner>
     </Suspense>
   );
 };
