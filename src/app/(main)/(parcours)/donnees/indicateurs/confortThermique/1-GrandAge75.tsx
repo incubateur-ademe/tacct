1f2d2257e90c887219221f5c37f25b833e8383c2
AntoineConegero <antoine.conegero@gmail.com>
2026-06-30 16:28:52 +0200

    chore: fix anchors + mobile Drom accordéon

--- src/app/(main)/(parcours)/donnees/indicateurs/confortThermique/1-GrandAge75.tsx
+++ src/app/(main)/(parcours)/donnees/indicateurs/confortThermique/1-GrandAge75.tsx
@@ -1,21 +1,21 @@
-"use client";
-import WarningIcon from "@/assets/icons/exclamation_point_icon_black.png";
+'use client';
+import WarningIcon from '@/assets/icons/exclamation_point_icon_black.png';
 import DataNotFound from '@/assets/images/no_data_on_territory.svg';
-import { LineChartGrandAge } from "@/components/charts/inconfortThermique/lineChartGrandAge";
-import { MicroCircleGrid } from "@/components/charts/MicroDataviz";
-import { ExportButton } from "@/components/exports/ExportButton";
-import DataNotFoundForGraph from "@/components/graphDataNotFound";
-import { Loader } from "@/components/ui/loader";
-import { CustomTooltipNouveauParcours } from "@/components/utils/Tooltips";
-import { Body } from "@/design-system/base/Textes";
-import { grandAgeMapper } from "@/lib/mapper/inconfortThermique";
-import { ConfortThermique } from "@/lib/postgres/models";
+import { LineChartGrandAge } from '@/components/charts/inconfortThermique/lineChartGrandAge';
+import { MicroCircleGrid } from '@/components/charts/MicroDataviz';
+import { ExportButton } from '@/components/exports/ExportButton';
+import DataNotFoundForGraph from '@/components/graphDataNotFound';
+import { Loader } from '@/components/ui/loader';
+import { CustomTooltipNouveauParcours } from '@/components/utils/Tooltips';
+import { Body } from '@/design-system/base/Textes';
+import { grandAgeMapper } from '@/lib/mapper/inconfortThermique';
+import { ConfortThermique } from '@/lib/postgres/models';
 import { GrandAgeText } from '@/lib/staticTexts';
-import { IndicatorExportTransformations } from "@/lib/utils/export/environmentalDataExport";
-import { eptRegex, numberWithSpacesRegex } from "@/lib/utils/regex";
+import { IndicatorExportTransformations } from '@/lib/utils/export/environmentalDataExport';
+import { eptRegex, numberWithSpacesRegex } from '@/lib/utils/regex';
 import { Round } from '@/lib/utils/reusableFunctions/round';
 import Image from 'next/image';
-import { useSearchParams } from "next/navigation";
+import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import styles from '../../explorerDonnees.module.scss';
 import { sumProperty } from '../fonctions';
@@ -31,18 +31,23 @@ export const GrandAge75 = ({
   const code = searchParams.get('code')!;
   const libelle = searchParams.get('libelle')!;
   const type = searchParams.get('type')!;
-  const libelleDepartement = type === "epci" ? confortThermique[0]?.libelle_departement : "";
-  const departement = type === "epci" ? confortThermique[0]?.departement : "";
-  const [multipleDepartements, setMultipleDepartements] = useState<string[]>([]);
-  const grandAgeMapped = confortThermique.map(
-    grandAgeMapper
+  const libelleDepartement =
+    type === 'epci' ? confortThermique[0]?.libelle_departement : '';
+  const departement = type === 'epci' ? confortThermique[0]?.departement : '';
+  const [multipleDepartements, setMultipleDepartements] = useState<string[]>(
+    []
   );
-  const territoireSup = type === 'epci' ? grandAgeMapped.filter((e) => e.departement === departement) : grandAgeMapped;
-  const percentTerritoireSup = Round((
+  const grandAgeMapped = confortThermique.map(grandAgeMapper);
+  const territoireSup =
+    type === 'epci'
+      ? grandAgeMapped.filter((e) => e.departement === departement)
+      : grandAgeMapped;
+  const percentTerritoireSup = Round(
     (100 * sumProperty(territoireSup, 'over_75_sum_2022')) /
-    (sumProperty(territoireSup, 'to_75_sum_2022') +
-      sumProperty(territoireSup, 'under_4_sum_2022'))
-  ), 2);
+      (sumProperty(territoireSup, 'to_75_sum_2022') +
+        sumProperty(territoireSup, 'under_4_sum_2022')),
+    2
+  );
   const grandAgeTerritoire =
     type === 'commune'
       ? grandAgeMapped.filter((e) => e.code_geographique === code)
@@ -53,7 +58,17 @@ export const GrandAge75 = ({
           : grandAgeMapped;
 
   const yData = GrandAge75LineChartYData(grandAgeTerritoire);
-  const xData = ['1968', '1975', '1982', '1990', '1999', '2006', '2011', '2016', '2022'];
+  const xData = [
+    '1968',
+    '1975',
+    '1982',
+    '1990',
+    '1999',
+    '2006',
+    '2011',
+    '2016',
+    '2022'
+  ];
   const yGraphData = Object.values(yData)
     .map(Number)
     .map((value) => (isNaN(value) ? null : value));
@@ -61,49 +76,59 @@ export const GrandAge75 = ({
     'Nombre de personnes de plus de 75 ans divisé par la population totale à chaque recensement INSEE.';
 
   useEffect(() => {
-    if (type === "epci" && code) {
-      const departements = confortThermique.map(item => item.departement);
+    if (type === 'epci' && code) {
+      const departements = confortThermique.map((item) => item.departement);
       const uniqueDepartements = Array.from(new Set(departements));
       setMultipleDepartements(uniqueDepartements);
     }
   }, [type, code, confortThermique]);
-  const exportData = IndicatorExportTransformations.confortThermique.GrandAge75(grandAgeTerritoire);
+  const exportData =
+    IndicatorExportTransformations.confortThermique.GrandAge75(
+      grandAgeTerritoire
+    );
 
   return (
     <>
       <div className={styles.datavizContainer}>
         <div className={styles.dataTextWrapper}>
           <div className={styles.chiffreDynamiqueWrapper}>
-            <MicroCircleGrid pourcentage={Number(yData.over_75_2022_percent)} arrondi={2} ariaLabel="Pourcentage des personnes âgées de plus de 80 ans" />
-            {
-              !Object.values(yData).slice(0, -2).includes('NaN') && (
-                <>
-                  <Body weight='bold' style={{ color: "var(--gris-dark)" }}>
-                    En 2022, <b>{numberWithSpacesRegex(yData.over_75_2022_percent)} %</b> de la
-                    population de votre territoire est constitué de personnes
-                    âgées de plus de 75 ans (soit{' '}
-                    <b>
-                      {numberWithSpacesRegex(sumProperty(
-                        grandAgeTerritoire,
-                        'over_75_sum_2022'
-                      ))}
-                    </b>{' '}
-                    personnes).
-                  </Body>
-                </>
-              )
-            }
-            {
-              type === "commune" && percentTerritoireSup && percentTerritoireSup !== "NaN" ? (
-                <Body weight='bold' style={{ color: "var(--gris-dark)" }}>
-                  Ce taux est de {percentTerritoireSup} % dans votre EPCI.
+            <MicroCircleGrid
+              pourcentage={Number(yData.over_75_2022_percent)}
+              arrondi={2}
+              ariaLabel="Pourcentage des personnes âgées de plus de 80 ans"
+            />
+            {!Object.values(yData).slice(0, -2).includes('NaN') && (
+              <>
+                <Body weight="bold" style={{ color: 'var(--gris-dark)' }}>
+                  En 2022,{' '}
+                  <b>{numberWithSpacesRegex(yData.over_75_2022_percent)} %</b>{' '}
+                  de la population de votre territoire est constitué de
+                  personnes âgées de plus de 75 ans (soit{' '}
+                  <b>
+                    {numberWithSpacesRegex(
+                      sumProperty(grandAgeTerritoire, 'over_75_sum_2022')
+                    )}
+                  </b>{' '}
+                  personnes).
                 </Body>
-              ) : type === "epci" && percentTerritoireSup && percentTerritoireSup !== "NaN" ? (
-                <Body weight='bold' style={{ color: "var(--gris-dark)" }}>
-                  Ce taux est de {percentTerritoireSup} % dans ce département : {libelleDepartement}.
-                </Body>
-              ) : ''
-            }
+              </>
+            )}
+            {type === 'commune' &&
+            percentTerritoireSup &&
+            percentTerritoireSup !== 'NaN' ? (
+              <Body weight="bold" style={{ color: 'var(--gris-dark)' }}>
+                Ce taux est de {percentTerritoireSup} % dans votre EPCI.
+              </Body>
+            ) : type === 'epci' &&
+              percentTerritoireSup &&
+              percentTerritoireSup !== 'NaN' ? (
+              <Body weight="bold" style={{ color: 'var(--gris-dark)' }}>
+                Ce taux est de {percentTerritoireSup} % dans ce département :{' '}
+                {libelleDepartement}.
+              </Body>
+            ) : (
+              ''
+            )}
             <CustomTooltipNouveauParcours title={methodeCalcul} />
             <GrandAgeText />
           </div>
@@ -113,46 +138,57 @@ export const GrandAge75 = ({
             <div
               style={{
                 backgroundColor: 'white',
-                height: !Object.values(yData).slice(0, -2).includes('NaN') ? '500px' : 'max-content',
+                height: !Object.values(yData).slice(0, -2).includes('NaN')
+                  ? '500px'
+                  : 'max-content',
                 width: '100%',
-                borderRadius: '1rem 0 0 0',
+                borderRadius: '1rem 0 0 0'
               }}
             >
-              {
-                !Object.values(yData).slice(0, -2).includes('NaN') ? (
-                  <>
-                    <LineChartGrandAge xData={xData} yData={yGraphData} disclaimer={multipleDepartements.length > 1} />
-                    {
-                      multipleDepartements.length > 1 &&
-                      <div style={{ minWidth: "450px", backgroundColor: "white", padding: "1em", transform: "translateY(-80px)" }}>
-                        <div className='flex flex-row items-center justify-center'>
-                          <Image
-                            src={WarningIcon}
-                            alt="Attention"
-                            width={24}
-                            height={24}
-                            style={{ marginRight: '0.5em', alignItems: 'center' }}
-                          />
-                          <Body style={{ fontSize: 12 }}>
-                            L'EPCI sélectionné s'étend sur
-                            plusieurs départements. La comparaison proposée est
-                            effectuée avec : {libelleDepartement}
-                          </Body>
-                        </div>
+              {!Object.values(yData).slice(0, -2).includes('NaN') ? (
+                <>
+                  <LineChartGrandAge
+                    xData={xData}
+                    yData={yGraphData}
+                    disclaimer={multipleDepartements.length > 1}
+                  />
+                  {multipleDepartements.length > 1 && (
+                    <div
+                      style={{
+                        minWidth: '450px',
+                        backgroundColor: 'white',
+                        padding: '1em',
+                        transform: 'translateY(-80px)'
+                      }}
+                    >
+                      <div className="flex flex-row items-center justify-center">
+                        <Image
+                          src={WarningIcon}
+                          alt="Attention"
+                          width={24}
+                          height={24}
+                          style={{ marginRight: '0.5em', alignItems: 'center' }}
+                        />
+                        <Body style={{ fontSize: 12 }}>
+                          L'EPCI sélectionné s'étend sur plusieurs départements.
+                          La comparaison proposée est effectuée avec :{' '}
+                          {libelleDepartement}
+                        </Body>
                       </div>
-                    }
-                  </>
-                )
-                  : <div className='p-10 flex flex-row justify-center'>
-                    <DataNotFoundForGraph image={DataNotFound} />
-                  </div>
-              }
+                    </div>
+                  )}
+                </>
+              ) : (
+                <div className="p-10 flex flex-row justify-center">
+                  <DataNotFoundForGraph image={DataNotFound} />
+                </div>
+              )}
             </div>
           ) : (
             <Loader />
           )}
           <SourceExport
-            anchor="Grand âge"
+            anchor="Grand-âge"
             exportComponent={
               <ExportButton
                 data={exportData}
@@ -163,7 +199,7 @@ export const GrandAge75 = ({
                 sheetName="Grand-âge"
               />
             }
-            source='INSEE, 2024 (consultée en février 2026)'
+            source="INSEE, 2024 (consultée en février 2026)"
             condition={!Object.values(yData).slice(0, -2).includes('NaN')}
           />
         </div>
