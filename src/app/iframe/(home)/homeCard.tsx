6b52ee98848baa6bd1e0978be799ca72c979a582
AntoineConegero <antoine.conegero@gmail.com>
2026-09-24 12:41:20 +0200

    chore(accessibilité): tags titles

--- src/app/iframe/(home)/homeCard.tsx
+++ src/app/iframe/(home)/homeCard.tsx
@@ -1,4 +1,4 @@
-import { Body, H4 } from '@/design-system/base/Textes';
+import { Body, H3 } from '@/design-system/base/Textes';
 import Image, { StaticImageData } from 'next/image';
 import styles from './home.module.scss';
 
@@ -22,7 +22,18 @@ export const HomeCard = ({
           className={styles.homeCardImage}
         />
       </div>
-      <H4 style={{ color: "var(--principales-vert)", margin: "0.5rem 0 1rem", fontSize: "1rem" }}>{titre}</H4>
+      <H3
+        style={{
+          color: "var(--principales-vert)",
+          margin: "0.5rem 0 1rem",
+          fontSize: "1rem",
+          fontWeight: 700,
+          lineHeight: "1.5rem",
+          letterSpacing: "0.85px",
+        }}
+      >
+        {titre}
+      </H3>
       <Body style={{ color: "#3D3D3D" }}>{description}</Body>
     </div>
   )
